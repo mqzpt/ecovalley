4516244c927e6ed5efb196a01cb5854d7ff825f1
Matthew Athanasopoulos <matthew.athanasopoulos@gmail.com>
2024-10-05 20:44:49 -0400

    feat: show amount used in kg per material

--- materials.py
+++ materials.py
@@ -30,7 +30,7 @@ def get_materials_for_product(user_prompt: str) -> dict:
     selected_list = co.generate(
         model="command-xlarge-nightly",
         prompt=f"You are a helpful assistant that selects materials for products. Do not provide any text outputs, only return a python list with no new lines or spaces. You may only choose products EXACTLY as they appear in the following list: {MATERIAL_NAMES}. Return a Python list of 3 dictionaries, each dictionary MUST contain MULTIPLE keys and values, where the keys are the materials in the proudct, and the values are the amount of each product in kilograms. Complete this task for the following product: {user_prompt}.",
-        max_tokens=250,
+        max_tokens=350,
         num_generations=1,
         temperature=0.5,
     )
@@ -52,9 +52,13 @@ def get_option_dataframes(material_dict) -> pd.DataFrame:
     for option in material_dict:
         option_df = materials_df.loc[materials_df['material'].isin(option.keys())]
         option_df['cost'] = round(option_df['price_per_kg'] * list(option.values()), 4)
+        
+        amount_used_series = pd.Series(list(option.values()))
+        option_df['amount_used_kg'] = amount_used_series.round(3)
+        
         option_df['energy_used_mj'] = round(option_df['energy_mj_per_kg'] * list(option.values()), 3)
         option_df['carbon_used_kg'] = round(option_df['carbon_kg_per_kg'] * list(option.values()), 3)
-        option_frames.append(option_df[['material', 'cost', 'energy_used_mj', 'carbon_used_kg']])
+        option_frames.append(option_df[['material', 'amount_used_kg', 'cost', 'energy_used_mj', 'carbon_used_kg']])
         
     return option_frames
         
